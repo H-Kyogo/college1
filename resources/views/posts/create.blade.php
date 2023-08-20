2acf13c119091a992452a377290b2f0976792760
H-Kyogo <K-hayashi0613@outlook.jp>
2023-08-21 06:05:42 +0900

    記事投稿画面2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,27 +3,22 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        
     </head>
-    <body class="antialiased">
+    <body>
         <h1>Blog Name</h1>
-       <from acttion="/posts" method="POST">
-           @csrf
-           <div class="title">
-               <h2>Title</h2>
-               <input type= "text" name=post[title] placeholedr= "タイトル">
-           </div>
-           <div class="body">
-               <h2>Body</h2>
-               <textarea name="post"[body]" placeholedr="今日も一日お疲れ様でした。"></textarea>
-           </div>
-           <input type="submit" value="store">
-       </from>
-       <div class='footer'>
-           
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル"/>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+            </div>
+            <input type="submit" value="store"/>
+        </form>
+        <div class="footer">
             <a href="/">戻る</a>
         </div>
     </body>
